1a1d58a7c05c9c0b97ce6222d883a5ff25b96733
Tatiana Osipova <otv120780@gmail.com>
2023-06-20 11:22:37 -0400

    feat: change name of repo

--- js/main.js
+++ js/main.js
@@ -21,10 +21,26 @@ if (ScrollTrigger.isTouch !== 1) {
     let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
 
     itemsL.forEach(item => {
-        gsap.fromTo(item, {x: -500, opacity: 0}, {
+        gsap.fromTo(item, {x: -150, opacity: 0}, {
             opacity: 1, x: 0,
             scrollTrigger: {
                 trigger: item,
+                start: '-850',
+                end: '-100',
+                scrub: true,
+            }
+        })
+    })
+
+    let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
+
+    itemsR.forEach(item => {
+        gsap.fromTo(item, {x: 150, opacity: 0}, {
+            opacity: 1, x: 0,
+            scrollTrigger: {
+                trigger: item,
+                start: '-850',
+                end: '-100',
                 scrub: true,
             }
         })
